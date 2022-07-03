4e0ed4f4b9369586ae541f53cf27be4e3906e682
Ferhat Eren Dalçık <f.dalcik@azerion.com>
2022-07-03 14:44:04 +0300

    fix gun rotation and movement

--- Assets/_Scripts/Core/GunController.cs
+++ Assets/_Scripts/Core/GunController.cs
@@ -9,7 +9,8 @@ public class GunController : MonoBehaviour
     public Transform gunBulletPoint;
     private Bullet currentBullet;
     private Rigidbody _rb;
-
+    private float _bulletSpeed = 60;
+    private float _torque = 120;
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
@@ -18,13 +19,14 @@ public class GunController : MonoBehaviour
     private void OnMouseDown()
     { 
         currentBullet = Instantiate(bullets, gunBulletPoint.position, gunBulletPoint.rotation); 
-        currentBullet.Movement(gunBulletPoint.forward *  40);
-
-    }
-
-    private void Update()
-    {
+        currentBullet.Movement(gunBulletPoint.forward *  _bulletSpeed);
+        
         var dir = Vector3.Dot(gunBulletPoint.forward, Vector3.right) < 0 ? Vector3.back : Vector3.forward;
-        _rb.AddTorque(dir * 10);
+        
+        float angularPoint = Mathf.InverseLerp(0, 10, Mathf.Abs(_rb.angularVelocity.z));
+        float amount = Mathf.Lerp(0, 150, angularPoint);
+        float torque = _torque + amount;
+
+        _rb.AddTorque(dir * torque);
     }
 }
